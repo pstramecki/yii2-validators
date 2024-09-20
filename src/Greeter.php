f5e9e6da628e4b5599fb2a63c5767de77f46e64c
p.stramecki <p.stramecki@gmail.com>
2024-09-20 16:48:32 +0200

    fix phpunit config path

--- src/Greeter.php
+++ src/Greeter.php
@@ -4,5 +4,11 @@ namespace pstramecki\validators;
 
 class Greeter
 {
+    public function greet(string $name = null):string {
+        if($name) {
+            return "Hello, $name!";
+        }
 
+        return "Hello!";
+    }
 }
