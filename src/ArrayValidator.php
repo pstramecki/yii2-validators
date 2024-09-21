55bf9ea63d13ba9fdec034c014aa796c62c1c17b
p.stramecki <p.stramecki@gmail.com>
2024-09-21 13:10:16 +0200

    prepare first validator

---
+++ src/ArrayValidator.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace pstramecki\validators;
+
+use yii\validators\Validator;
+
+class ArrayValidator extends Validator
+{
+
+}
---
+++ tests/ArrayValidatorTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace pstramecki\validators\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ArrayValidatorTest extends TestCase
+{
+
+}
