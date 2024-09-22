d8ce20369491d931ac9ad676dec19267e5965bde
p.stramecki <p.stramecki@gmail.com>
2024-09-22 22:04:43 +0200

    add first validator - array validator some fixes for phpunit

--- src/ArrayValidator.php
+++ src/ArrayValidator.php
@@ -2,9 +2,23 @@
 
 namespace pstramecki\validators;
 
+use Yii;
 use yii\validators\Validator;
 
-class ArrayValidator extends Validator
+final class ArrayValidator extends Validator
 {
+    public function init(): void
+    {
+        parent::init();
 
+        if ($this->message === null) {
+            $this->message = Yii::t('yii', '{attribute} must be an array.');
+        }
+    }
+
+
+    protected function validateValue($value): ?array
+    {
+        return is_array($value) ? null : [$this->message, []];
+    }
 }
--- tests/ArrayValidatorTest.php
+++ tests/ArrayValidatorTest.php
@@ -2,9 +2,58 @@
 
 namespace pstramecki\validators\tests;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use pstramecki\validators\ArrayValidator;
+use ReflectionClass;
 
-class ArrayValidatorTest extends TestCase
+final class ArrayValidatorTest extends TestCase
 {
+    public function testIsFinal()
+    {
+        $reflector = new ReflectionClass(ArrayValidator::class);
+        $this->assertTrue($reflector->isFinal());
+    }
 
+
+    #[DataProvider('validValues')]
+    public function testValidateIsValid($value): void
+    {
+
+        $validator = new ArrayValidator();
+
+        $this->assertTrue($validator->validate($value));
+    }
+
+    #[DataProvider('invalidValues')]
+    public function testValidateIsInvalid($value): void
+    {
+
+        $validator = new ArrayValidator();
+
+        $this->assertFalse($validator->validate($value));
+    }
+
+    public static function validValues(): array
+    {
+        return [
+            [[]],
+            [array()],
+            [[1, 2, 3, 4]],
+            [[1, 'a', '2', 3, []]],
+            [['1' => 'foo', '2' => 'bar']],
+            [['foo' => 1, 'bar' => 2]],
+            [[new \stdClass(), new \stdClass()]],
+        ];
+    }
+
+    public static function invalidValues(): array
+    {
+        return [
+            ['a'],
+            ['foo'],
+            [new \stdClass()],
+            [1],
+        ];
+    }
 }
